5187239c27abc69df8db1424699afd116ffe109a
Felix <felix.wielander@gmail.com>
2021-07-18 13:50:01 +0200

    Example Project (#13)
    * Created empty example project
    
    * Added tinycolor2 as dependency of example/
    
    * Created ColorChooser widget
    
    * Adapted app and main page to contain color chooser
    
    * Moved ColorChooser to folder widgets/
    
    * Created ApplyMethodItem widget
    
    * Changed ColorChooser color management
    
    * Improved ApplyMethodItem
    
    * Added some example ApplyMethodItems
    
    * Added all transformation functions as method items to page
    
    * Added flutter version bounds to example pubspec.yaml
    
    * Removed not needed cupertino_icons dependency in example
    
    * Updated import in example's main.dart
    
    * Replaced jcenter with mavenCentral in build.gradle
    
    * Fixed build.gradle: jcenter -> mavenCentral
    
    * Added trailing commas in apply_method_item.dart
    
    * Removed widget files
    
    * Made example main.dart simple
    
    * Use other values for color transformation functions
    
    * Introduced _createListItem method
    
    * Added greyscale, compliment and mix examples
    
    * Remove scroll view and change column to listview
    
    * Modified list items to show more info
    
    * Added example dirs to .idea/tinycolor2.iml
    
    * Added c for extension usage
    
    * Use """ instead of \n
    
    * Use grey as theme color
    
    * Make tc final
    
    * Added const
    
    * Updated example/README.md
    
    * Removed example/readme null-safety stuff

---
+++ example/README.md
@@ -0,0 +1,13 @@
+# example
+
+This example project shows some basic usages of the `tinycolor2` package. 
+
+## Run project
+
+### In your favourite  IDE
+Open `tinycolor2/example` in your favourite IDE and run it from there.
+
+### From terminal
+Navigate to `tinycolor2/example` in a terminal, then run:
+1. `flutter pub get`
+2. `flutter run`
---
+++ example/lib/main.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+import 'package:tinycolor2/tinycolor2.dart';
+
+void main() {
+  runApp(TinyColorApp());
+}
+
+class TinyColorApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'TinyColor2 Example',
+      theme: ThemeData(
+        primarySwatch: Colors.grey,
+      ),
+      home: ExamplePage(),
+    );
+  }
+}
+
+class ExamplePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final TinyColor tc = TinyColor(Colors.blue);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("TinyColor2 Example"),
+      ),
+      body: ListView(
+        children: <Widget>[
+          _createListItem(
+            title: "Original color",
+            subtitle: """TinyColor tc = TinyColor(Colors.blue)
+Color c = Colors.blue""",
+            color: tc.color,
+            showOr: false,
+          ),
+          _createListItem(
+            title: "tc.lighten(20)",
+            subtitle: "c.lighten(20)",
+            color: tc.lighten(20).color,
+          ),
+          _createListItem(
+            title: "tc.brighten(20)",
+            subtitle: "c.brighten(20)",
+            color: tc.brighten(20).color,
+          ),
+          _createListItem(
+            title: "tc.darken(20)",
+            subtitle: "c.darken(20)",
+            color: tc.darken(20).color,
+          ),
+          _createListItem(
+            title: "tc.tint(20)",
+            subtitle: "c.tint(20)",
+            color: tc.tint(20).color,
+          ),
+          _createListItem(
+            title: "tc.shade(20)",
+            subtitle: "c.shade(20)",
+            color: tc.shade(20).color,
+          ),
+          _createListItem(
+            title: "tc.desaturate(20)",
+            subtitle: "c.desaturate(20)",
+            color: tc.desaturate(20).color,
+          ),
+          _createListItem(
+            title: "tc.saturate(20)",
+            subtitle: "c.saturate(20)",
+            color: tc.saturate(20).color,
+          ),
+          _createListItem(
+            title: "tc.greyscale()",
+            subtitle: "c.greyscale()",
+            color: tc.greyscale().color,
+          ),
+          _createListItem(
+            title: "tc.spin(90)",
+            subtitle: "c.spin(90)",
+            color: tc.spin(90).color,
+          ),
+          _createListItem(
+            title: "tc.compliment()",
+            subtitle: "c.complement()",
+            color: tc.complement().color,
+          ),
+          _createListItem(
+            title: "tc.mix(input: Colors.yellow, amount: 20)",
+            subtitle: "c.mix(Colors.yellow, 20)",
+            color: tc.mix(input: Colors.yellow, amount: 20).color,
+          ),
+        ],
+      ),
+    );
+  }
+
+  _createListItem({
+    required String title,
+    required String subtitle,
+    required Color color,
+    bool showOr = true,
+  }) =>
+      ListTile(
+        title: Text(title),
+        subtitle: Text("${showOr ? "or: " : ""}$subtitle"),
+        trailing: CircleAvatar(backgroundColor: color),
+      );
+}
